4564d60b2a3fc845fedc66e2890c7fb5a6fa7b53
Enno T. Boland <g@s01.de>
2025-06-18 09:48:08 +0200

    librope: add additional unit tests

--- subprojects/librope/test/cursor.c
+++ subprojects/librope/test/cursor.c
@@ -135,12 +135,42 @@ cursor_event() {
 	ASSERT_EQ(0, rv);
 	rv = rope_cursor_cleanup(&c2);
 	ASSERT_EQ(0, rv);
-	rv = rope_cleanup(&r);
-	ASSERT_EQ(0, rv);
+        rv = rope_cleanup(&r);
+        ASSERT_EQ(0, rv);
+}
+
+static void
+cursor_move_codepoint() {
+        int rv = 0;
+        struct Rope r = {0};
+        rv = rope_init(&r);
+        ASSERT_EQ(0, rv);
+
+        rv = rope_append_str(&r, "ABC");
+        ASSERT_EQ(0, rv);
+
+        struct RopeCursor c = {0};
+        rv = rope_cursor_init(&c, &r);
+        ASSERT_EQ(0, rv);
+
+        rv = rope_cursor_move_to_index(&c, 1);
+        ASSERT_EQ(0, rv);
+        int32_t cp = rope_cursor_codepoint(&c);
+        ASSERT_EQ('B', cp);
+
+        rv = rope_cursor_move(&c, -2);
+        ASSERT_EQ(0, rv);
+        ASSERT_EQ((size_t)0, c.index);
+
+        rv = rope_cursor_cleanup(&c);
+        ASSERT_EQ(0, rv);
+        rv = rope_cleanup(&r);
+        ASSERT_EQ(0, rv);
 }
 
 DECLARE_TESTS
 TEST(cursor_basic)
 TEST(cursor_utf8)
 TEST(cursor_event)
+TEST(cursor_move_codepoint)
 END_TESTS
--- subprojects/librope/test/iterator.c
+++ subprojects/librope/test/iterator.c
@@ -21,6 +21,47 @@ test_iterator_simple() {
 	rv = rope_range_cleanup(&range);
 }
 
+static void
+test_iterator_collect() {
+        int rv = 0;
+        struct Rope r = {0};
+        rv = rope_init(&r);
+        ASSERT_EQ(0, rv);
+
+        rv = rope_append_str(&r, "Hello ");
+        ASSERT_EQ(0, rv);
+        rv = rope_append_str(&r, "World");
+        ASSERT_EQ(0, rv);
+
+        struct RopeRange range = {0};
+        rv = rope_range_init(&range, &r, NULL, NULL, NULL);
+        ASSERT_EQ(0, rv);
+        rv = rope_cursor_move_to_index(rope_range_end(&range), rope_char_size(&r));
+        ASSERT_EQ(0, rv);
+
+        struct RopeIterator iter = {0};
+        rv = rope_iterator_init(&iter, &range);
+        ASSERT_EQ(0, rv);
+
+        char result[32] = {0};
+        size_t offset = 0;
+        const uint8_t *value = NULL;
+        size_t size = 0;
+        while (rope_iterator_next(&iter, &value, &size)) {
+                memcpy(&result[offset], value, size);
+                offset += size;
+        }
+        ASSERT_STREQ("Hello World", result);
+
+        rv = rope_iterator_cleanup(&iter);
+        ASSERT_EQ(0, rv);
+        rv = rope_range_cleanup(&range);
+        ASSERT_EQ(0, rv);
+        rv = rope_cleanup(&r);
+        ASSERT_EQ(0, rv);
+}
+
 DECLARE_TESTS
 TEST(test_iterator_simple)
+TEST(test_iterator_collect)
 END_TESTS
